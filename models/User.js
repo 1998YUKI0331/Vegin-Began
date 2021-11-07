878586649dfcf7e721bc51ebef477db4718fe16b
1998YUKI0331 <dkenl0110@dongguk.edu>
2021-11-07 17:15:49 +0900

    protect user pwd using pbkdf2

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ var mongoose = require("mongoose");
 
 var UserSchema = new mongoose.Schema({
     username: String, //아이디
-    password: String, //비밀번호
+    password: String, //비밀번호 (해쉬값)
+    salt: String, //비밀번호 암호화
     likeList: [String] //찜목록
 });
 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 const path = require('path');
 const session = require('express-session');
-const crypto = require('crypto');
+const crypto = require('crypto'); // 유저 비밀번호 암호화
 const FileStore = require('session-file-store')(session); // 세션을 파일에 저장
 const cookieParser = require('cookie-parser');
 const xlsx = require('xlsx');
@@ -41,7 +41,7 @@ app.get("/", (req, res) => {
 		res.render('index', { is_logined: req.session.is_logined, username: req.session.username });
 	} else{ res.render('index',{ is_logined: false });
 	}
- });
+});
 app.get("/signup", (req, res) => { res.render('signup'); });
 app.get("/login", (req, res) => { res.render('login'); });
 app.get('/logout',(req, res) => { req.session.destroy(function(err){ res.redirect('/'); });});
@@ -50,38 +50,49 @@ app.get("/board", (req, res) => {
 		res.render('board', { is_logined: req.session.is_logined, username: req.session.username });
 	} else{ res.render('board',{ is_logined: false });
 	}
- });
- app.get("/content", (req, res) => { 
+});
+app.get("/content", (req, res) => { 
 	if(req.session.is_logined == true) {
 		res.render('content', { is_logined: req.session.is_logined, username: req.session.username });
 	} else{ res.render('content',{ is_logined: false });
 	}
- });
- 
-app.post('/signup', (req, res) => {
-	var new_user = new Users(req.body);
+});
 
-	new_user.save((err) => {
-		if (err) return res.status(500).json({ message: '저장 실패!' });
-		else {
-			res.redirect('/login');
-		}
-	});
+app.post('/signup', (req, res) => {
+	crypto.randomBytes(64, (err, buf) => {
+		const salt = buf.toString('base64');
+		crypto.pbkdf2(req.body.password, salt, 100000, 64, 'sha512', (err, key) => {
+			const hash = key.toString('base64');
+			var new_user = new Users({ username: req.body.username, password: hash, salt: salt});
+
+			new_user.save((err) => {
+				if (err) return res.status(500).json({ message: '저장 실패!' });
+				else {
+					res.redirect('/login');
+				}
+			});
+		})
+	})
 });
 
 app.post("/login", (req, res) => { 
-	Users.findOne({ username: req.body.username, password: req.body.password }, (err, user) => {
+	Users.findOne({ username: req.body.username }, (err, user) => {
 		if (err) return res.status(500).json({ message: '에러!' });
 		else if (user) {
-			req.session.is_logined = true;
-			req.session.username = req.body.username;
-			req.session.password = req.body.password;
-			req.session.save(function(){ 
-			res.render('index', { // 로그인 정보 전달
-					username: req.body.username,
-					password: req.body.password,
-					is_logined : true
-				});
+			crypto.pbkdf2(req.body.password, user.salt, 100000, 64, "sha512", (err, key) => {
+				const hash = key.toString("base64");
+				if (hash === user.password) {
+					req.session.is_logined = true;
+					req.session.username = req.body.username;
+					req.session.save(function(){ 
+						res.render('index', { // 로그인 정보 전달
+							username: req.body.username,
+							is_logined : true
+						});
+					});
+				} else {
+					return res.status(404).json({ message: '비밀번호 틀림!' });
+				}
 			});
 		}
 		else return res.status(404).json({ message: '유저 없음!' });
